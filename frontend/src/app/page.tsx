f25ad99ff2ffb8c68d96d3940be3cba8530a30a2
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2025-03-12 01:22:59 +0545

    shad cn ui library integrated for better visualization and responsiveness

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
+      <Button>Click me</Button>
     </h1>
   )
 }
