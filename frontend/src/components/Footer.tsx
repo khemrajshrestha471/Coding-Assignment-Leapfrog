f9b84eae10cef2de2ec0b753a697d5e09afb920d
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2025-03-15 17:15:17 +0545

    space alignment updated in footer section

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-6 text-center">
         <p className="text-sm">
-          <a href="https://www.khemrajshrestha.com.np/">Khem Raj Shrestha</a>
+          <a href="https://www.khemrajshrestha.com.np/">Khem Raj Shrestha </a>
           &copy; {new Date().getFullYear()} NoteX. All rights reserved.
         </p>
       </div>
